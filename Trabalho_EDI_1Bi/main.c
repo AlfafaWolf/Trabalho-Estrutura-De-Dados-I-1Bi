bb512b5a3231defe976e55975ecb9773dffc2b67
Gabriel Alejandro L. M <35781604+AlfafaWolf@users.noreply.github.com>
2019-03-28 00:20:47 -0300

    Anx/Inic ListaDE e testeListaDE
    + inicListaDE
    + anxListaDE
    + testeListaDE
    + Correção de diretório Lista DE e SE, .c e .h

--- Trabalho_EDI_1Bi/main.c
+++ Trabalho_EDI_1Bi/main.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 100
 #include "listaDE.c"
 #include "listaSE.c"
 
+void testListaDE();
 int main()
 {
-    printf("Hello world!\n");
+    printf("Hello world!\n\n");
+
+    testListaDE();
+
     return 0;
+}
+
+void testListaDE()
+{
+    printf("=============== TESTE LISTA_DE ===============\n");
+    ListaDE listaDE = inicListaDE();
+    anxListaDE(listaDE,"Cidade 1");
+    anxListaDE(listaDE,"Cidade 2");
+    anxListaDE(listaDE,"Cidade 3");
+
+    printf("> Lista de Cidades:\n");
+    printf("[|");
+    for(primListaDE(listaDE); !fimListaDE(listaDE); segListaDE(listaDE))
+    {
+        printf(" %s |", listaDE->iterador->info);
+    }
+    printf("]\n");
+    free(listaDE);
+    printf("==============================================\n");
 }
